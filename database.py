52ee63a2de4109bf10ef820f8ab5b76ac25b42fc
jai <solitaire@tygger.net>
2016-11-14 22:38:36 +1100

    add plugins

---
+++ database.py
@@ -0,0 +1,35 @@
+###########
+# Database handling for lioness
+####
+import MySQLdb
+
+
+class DataBase():
+	conn = ''
+	def __init__(self, dbname, username, mypass):
+		self.conn = MySQLdb.connect(user=username, passwd=mypass, db=dbname )
+
+	def show_tables(self):
+		return self.query("""SHOW TABLES;""", ())
+		
+
+	def query(self, query, holders):
+		#print("qing {}".format(query))
+
+		if (len(holders) == 0):
+			self.conn.query(query)
+			result = self.conn.use_result()
+			r = result.fetch_row(0)
+			#print(r)
+			return r
+		else:
+			#print("++++++++++++\n")
+			#print(query)
+			#print(holders)
+			#print("++++++++++++\n")
+			
+			c = self.conn.cursor()
+			c.execute(query, holders)
+			r =  c.fetchall()
+			self.conn.commit()
+			return r
---
+++ plugins/jargon.py
@@ -0,0 +1,50 @@
+########
+# Fetches jargon file entries
+######
+from plugins.base import PluginResponse, Plugin
+import sys
+import requests
+import html2text
+
+MAXLINES = 12
+
+class jargon(Plugin):
+	def __init__(self, dbconn):
+		self.keyword = "jargon"
+		self.response = PluginResponse()
+
+	def command(self, text):
+		self.response.setText("Nope")
+		if (text !=' '):
+			searchstr = "-".join(text.split())
+		
+
+			cap = searchstr[0].upper()
+			url = "http://www.catb.org/jargon/html/{}/{}.html".format(cap, searchstr)
+			#print(url)
+			resp = requests.get(url)
+			if (resp.status_code == 200):
+			#print(resp)
+				txt = html2text.html2text(resp.text).split("\n")
+				ftxt = ''
+
+				txt = txt[6:-6]
+				if (len(txt) > MAXLINES):
+					txt = txt[:MAXLINES]
+			
+					txt[-1] += "\n(more ...)\n "
+			
+				for i in (txt):
+					print(".{}.".format(i))
+					if (i.strip() != ''):
+						ftxt += i.strip() + "\n"
+				
+
+				ftxt += ("\n" + url)
+				self.response.setText(ftxt)
+			
+				
+			else:
+
+				self.response.setText("I couldn't find that url!")
+		return self.response
