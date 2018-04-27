051dba84247bbf069832e3dc8692ad9521accebd
jai <solitaire@tygger.net>
2018-04-27 20:34:58 +1000

    Better handling and listing

--- plugins/typehandler.py
+++ plugins/typehandler.py
@@ -9,7 +9,7 @@ import html2text
 
 class store(Plugin):
     def __init__(self, dbconn):
-        self.keyword = ("handle",)
+        self.keyword = ("handle","files")
         self.response = PluginResponse()
         self.error = ""
         self.dbconn = dbconn
@@ -20,46 +20,47 @@ class store(Plugin):
         self.response.setText("Nope")
         text = args.text.split()
         userID = args.user["user"]["id"]
-        tag = text[0]
-        url = text[1]
-        fileID = text[-1]
-        stored = url
-        if (tag == 'file_share'):
-            print("File: {0} {1} opts {2} ".format(url, fileID, args.opt))
+        if args.command == "handle":
             if (args.opt == 0):
-                print("\n\nOpted out - not storing\n\n")
-            else:
-                print("\n\nOpted no worries\n\n")
-            try:
-                resp = requests.get(url, headers={"Authorization":"Bearer {}".format(os.environ["BOT_TOKEN"])}, stream=True)
-                resp.raise_for_status()
+                self.response.setText("\n\nOpted out - not storing\n\n")
+                return self.response; 
+            tag = text[0]
+            url = text[1]
+            fileID = text[-1]
+            stored = url
+            
 
-                if resp.ok:
-                    filedir = "files/" + userID + "/"
-                    print("File: {0} ".format(filedir, fileID))
-                    if not os.path.exists(filedir):
-                        os.makedirs(filedir)
-                    with open(filedir + fileID , 'wb') as f:
-                        for block in resp.iter_content(1024):
-                            f.write(block)
-                        f.close()
-                else:
-                    print("Not ok")
-                print("saved")
+            if (tag == 'file_share'):
+                try:
+                    resp = requests.get(url, headers={"Authorization":"Bearer {}".format(os.environ["BOT_TOKEN"])}, stream=True)
+                    resp.raise_for_status()
 
-            except Exception as e:
-                print("No response! {}".format(e) )
+                    if resp.ok:
+                        filedir = "files/" + userID + "/"
+                        if not os.path.exists(filedir):
+                            os.makedirs(filedir)
+                        with open(filedir + fileID , 'wb') as f:
+                            for block in resp.iter_content(1024):
+                                f.write(block)
+                            f.close()
+
+                except Exception as e:
+                    self.response.setText("No response! {}".format(e) )
                 
 
+            try:
+                # replace with function
+                self.error = self.dbconn.query("""INSERT INTO `store`(`text`, `tag`, `userID`) VALUES(%s, %s, %s)""", (stored, tag, userID))
+                self.response.setText("Added for {}!".format(args.user["user"]["name"]))
+            except:
+                self.response.setText("Cannot la: {}".format(self.error))
 
-
-
-        try:
-            # replace with function
-            self.error = self.dbconn.query("""INSERT INTO `store`(`text`, `tag`, `userID`) VALUES(%s, %s, %s)""", (stored, tag, userID))
-            self.response.setText("Added for {}!".format(args.user["user"]["name"]))
-        except:
-            self.response.setText("Cannot la: {}".format(self.error))
+        elif args.command == "files":
+                self.error = self.dbconn.query("""SELECT text from `store` WHERE userID = %s AND `tag`=%s""" , (userID,"file_share"))
+                resp = ""
+                for myfile in self.error:
+                    resp += myfile[0] + "\n"
+                self.response.setText(resp)
 
 
         return self.response
